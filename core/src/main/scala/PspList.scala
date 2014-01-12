78c55cf50691d06c16ccdf049a77a7b3f1bc4823
Paul Phillips <paulp@improving.org>
2014-01-12 10:51:51 -0800

    Reasonable printing for PspList.

--- core/src/main/scala/PspList.scala
+++ core/src/main/scala/PspList.scala
@@ -15,6 +15,15 @@ object PspList {
   def empty[A] = nil.castTo[PspList[A]]
   def fill[A](n: Int)(body: => A): PspList[A] = apply(0 until n map (_ => body): _*)
   def apply[A](xs: A*): PspList[A]            = xs.foldRight(nil[A]())(_ :: _)
+
+  def stringOf[A](xs: PspList[A], max: Int = 7): String = {
+    def loop(n: Int, xs: PspList[A], res: String): String = (
+      if (n <= 0 || xs.isEmpty) res + ( if (xs.isEmpty) " :: nil" else " :: ..." )
+      else if (res == "") loop(n - 1, xs.tail, xs.head.to_s)
+      else loop(n - 1, xs.tail, res + " :: " + xs.head.to_s)
+    )
+    loop(max, xs, "")
+  }
 }
 
 sealed trait PspList[A] extends AnyRef with PspLinear[A] {
@@ -53,6 +62,8 @@ sealed trait PspList[A] extends AnyRef with PspLinear[A] {
     }
     loop(this)
   }
+
+  final override def toString = if (isEmpty) "nil" else PspList.stringOf(this)
 }
 
 final case object nil extends PspList[Nothing] {
@@ -62,10 +73,7 @@ final case object nil extends PspList[Nothing] {
 
   def apply[A](): PspList[A] = this.castTo[PspList[A]]
   def unapply[A](xs: PspList[A]): Boolean = this eq xs
-
-  override def toString = "nil"
 }
 final case class ::[A](head: A, tail: PspList[A]) extends PspList[A] {
   def isEmpty = false
-  override def toString = "cons"
 }
