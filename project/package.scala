936e15795005b01ac7c8778394d73b3dabc6b4d1
Paul Phillips <paulp@improving.org>
2014-08-21 09:18:51 -0700

    Import some sbt aliases.

--- project/package.scala
+++ project/package.scala
@@ -1,8 +1,19 @@
 package psp
 
 import sbt._, Keys._
+import sbt.complete.Parser
 
 package object build {
+  type ParserOf[A]    = Def.Initialize[State => Parser[A]]
+  type SettingOf[A]   = Def.Initialize[A]
+  type TaskOf[A]      = Def.Initialize[Task[A]]
+  type InputTaskOf[A] = Def.Initialize[InputTask[A]]
+  type SettingSeq     = Seq[Setting[_]]
+  type ScopedKeySeq   = Seq[Def.ScopedKey[_]]
+  type UnscopedKeySeq = Seq[AttributeKey[_]]
+  type DirToFiles     = File => PathFinder
+  type DependsRef     = ClasspathDep[ProjectReference]
+
   implicit def implicitStateOps(s: State): Stated = new Stated(s)
 
   def doto[A](x: A)(f: A => Unit): A = try x finally f(x)
