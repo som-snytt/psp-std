0e633bc156e1a83c7e884e6b7f3edd0fd363220d
Paul Phillips <paulp@improving.org>
2014-08-22 15:41:50 -0700

    Removing unused api.

--- view/src/main/scala/api/View.scala
+++ view/src/main/scala/api/View.scala
@@ -40,29 +40,11 @@ trait TypeConstructors[+A] extends Any with Foreach[A] {
   type Joined[+X]
 }
 
-trait ClusterView[+A] extends Any with TypeConstructors[A] {
-  type Cluster[+X]
-
-  def sliding(size: Size, step: Int): MapTo[Cluster[A]]
-  def groupBy[B](f: A => B): MapTo[(B, Cluster[A])]
-}
-
 trait MapElementView[+A] extends Any with TypeConstructors[A] {
   def map[B](f: A => B): MapTo[B]
   def flatten[B](implicit ev: A <:< Input[B]): MapTo[B]
   def flatMap[B](f: A => Input[B]): MapTo[B]
   def collect[B](pf: A ?=> B): MapTo[B]
-}
-
-trait DirectAccessView[+A] extends Any with TypeConstructors[A] {
-  def indexWhere(p: Predicate[A]): Index
-  def lastIndexWhere(p: Predicate[A]): Index
-}
-
-trait InvariantView[A] extends Any with TypeConstructors[A] {
-  def contains(elem: A): Boolean
-  def indexOf(elem: A): Index
-  def lastIndexOf(elem: A): Index
 }
 
 trait IsoView[+A] extends Any with TypeConstructors[A] {
@@ -81,16 +63,4 @@ trait IsoView[+A] extends Any with TypeConstructors[A] {
   def labeled(label: String): This
   def sized(size: Size): This
   def reverse: This
-}
-
-trait SplitView[A] extends Any with TypeConstructors[A] {
-  def span(p: Predicate[A]): Split[A]
-  def partition(p: Predicate[A]): Split[A]
-  def splitAt(p: Predicate[A]): Split[A]
-}
-
-trait ZipView[+A] extends Any with TypeConstructors[A] {
-  def zip[B](that: Input[B]): Joined[B]
-  def zipWith[B, C](that: Input[B])(f: (A, B) => C): MapTo[C]
-  def corresponds[B](that: Input[B])(p: Predicate2[A, B]): Boolean
 }
