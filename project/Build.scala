d6f755876dac306a70e0556cd3da183808cc4799
Paul Phillips <paulp@improving.org>
2014-08-29 17:12:54 -0700

    Disambiguate console startup.

--- project/Build.scala
+++ project/Build.scala
@@ -16,10 +16,10 @@ object Build extends sbt.Build with Versioning {
 
   def imports = """
     import scala.collection.{ mutable, immutable }
-    import psp.std._, api._, ansi._
+    import psp.std._, ansi._
     implicit final class ReplForeachOps[A](val target: Foreach[A]) {
       def !> : Unit = println(target mkString EOL)
-      def  >(implicit shows: Show[A]): Unit = println(target join EOL)
+      def  >(implicit shows: Show[A]): Unit = println(target.joinLines)
     }
     implicit final class ReplOps[A](val target: A) {
       def !> : Unit = println(target)
