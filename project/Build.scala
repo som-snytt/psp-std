ceaa91a1a52ca28b719b712062c4248a357ed52c
Paul Phillips <paulp@improving.org>
2014-09-02 09:23:47 -0700

    Trying hard to simplify the sbt project.
    Threw out sxr in the process.

--- project/Build.scala
+++ project/Build.scala
@@ -7,105 +7,66 @@ import com.typesafe.tools.mima.plugin.MimaKeys._
 import com.typesafe.tools.mima.plugin.MimaPlugin.mimaDefaultSettings
 import psp.meta._
 
-object Build extends sbt.Build with Versioning with ConsoleOnly with TestOnly {
+object Build extends sbt.Build with PublishOnly with ConsoleOnly with TestOnly {
   // scala.reflect.runtime.currentMirror.staticPackage("psp.core")
-  def opts = if (sys.props contains "debug") Seq("-Ylog:all") else Nil
-
-  def ifBinary[A](version: String)(thenp: => A, elsep: => A): TaskOf[A] =
-    scalaBinaryVersion map { case `version` => thenp ; case _ => elsep }
-
-  def inAllProjects[T](projects: => Seq[ProjectReference], key: SettingKey[T]): Project.Initialize[Seq[T]] = Def settingDyn {
-    val lb = loadedBuild.value
-    val pr = thisProjectRef.value
-    def resolve(ref: ProjectReference): ProjectRef = Scope.resolveProjectRef(pr.build, Load.getRootProject(lb.units), ref)
-    val refs = projects flatMap { base => Defaults.transitiveDependencies(resolve(base.project), lb, includeRoot = true, classpath = true, aggregate = true) }
-    refs map (ref => (key in ref).?) joinWith (_ flatMap { x => x })
+  def versionScalacOptions(binaryVersion: String): Seq[String] = {
+    val xs1 = if (sys.props contains "debug") Seq("-Ylog:all") else Nil
+    val xs2 = Seq("-Ywarn-dead-code", "-language:_")
+    val xs3 = binaryVersion match {
+      case "2.10" => Nil
+      case "2.11" => Seq("-Ywarn-unused", "-Ywarn-unused-import")
+    }
+    xs1 ++ xs2 ++ xs3
   }
-
-  def deepTasks[T](scoped: TaskKey[Seq[T]]): TaskOf[Seq[T]] =
-    deep(scoped.task)(_.join map (_.flatten.distinct))
-
-  def deep[T](scoped: SettingKey[T]): SettingOf[Seq[T]] =
-    inAllProjects(subprojects map (p => LocalProject(p.id)), scoped)
-
-  import Sxr._
-  def fullSxrSettings: SettingSeq = Sxr.settings ++ Seq(
-      libraryDependencies +=  "org.improving" %% "sxr" % "1.0.1" % SxrConfig.name,
-           sources in sxr <<= deepTasks(sources in Compile),
-     sxrSourceDirectories <<= deep(sourceDirectories in Compile).map(_.flatten), // to properly relativize the source paths
-     fullClasspath in sxr <<= deepTasks(externalDependencyClasspath in Compile)
-  )
-  def common = bintraySettings ++ mimaDefaultSettings ++ Seq[Setting[_]](
-                                resolvers  +=  "bintray/paulp" at "https://dl.bintray.com/paulp/maven",
-                             organization  :=  pspOrg,
-                             scalaVersion  :=  "2.11.2",
-                                  version  :=  "0.3.0-M8",
-                              logBuffered  :=  false,
-                            scalacOptions ++=  opts ++ Seq("-Ywarn-dead-code", "-language:_"),
-                            scalacOptions <++= ifBinary("2.11")("-Ywarn-unused" :: "-Ywarn-unused-import" :: Nil, Nil),
-                             javacOptions ++=  Seq("-nowarn", "-XDignore.symbol.file"),
-                                 licenses  :=  Seq("Apache-2.0" -> url("http://www.apache.org/licenses/LICENSE-2.0")),
-                  publishArtifact in Test  :=  false,
-                parallelExecution in Test  :=  false,
-                             fork in Test  :=  true,
-      scalacOptions in console in Compile  :=  Seq("-language:_"),
-    unmanagedSourceDirectories in Compile  +=  (sourceDirectory in Compile).value / s"scala${scalaBinaryVersion.value}",
-       unmanagedSourceDirectories in Test  +=  (sourceDirectory in Test).value / s"scala${scalaBinaryVersion.value}"
-  )
-
   def subprojects = List(api, std)
 
-  /** mima command optionally takes a baseline version, e.g. sbt 'mima 0.1.0-M1'
-   */
-  def mimaCommand = stateCommand {
-    case (s, Nil)            => mimaRun(s, stdArtifact("0.3.0-M5"))
-    case (s, version :: Nil) => mimaRun(s, stdArtifact(version))
-    case (s, _)              => s.fail
-  }
-
-  private def mimaRun(state: State, module: ModuleID): Unit =
-    state.put(previousArtifact in std, Some(module)) runTask (reportBinaryIssues in std)
-
-  lazy val root = project.root aggregate (api, std) settings (
-                           name :=  "psp-std-root",
-                    description :=  "psp's project which exists to please sbt",
-             crossScalaVersions :=  List("2.10.4", "2.11.2"),
-                    shellPrompt :=  (s => "%s#%s> ".format(name.value, (Project extract s).currentRef.project)),
-           aggregate in publish :=  false,
-      aggregate in publishLocal :=  false,
-                publishArtifact :=  false,
-                        publish <<= runPublish(publish),
-                   publishLocal <<= runPublish(publishLocal),
-                       commands +=  Command.args("mima", "<version>")(mimaCommand),
-                        console :=  (console in Compile in consoleOnly).value,
-                           test :=  (test in testOnly).value
-  )
-
   implicit class ProjectOps(val p: Project) {
-    def root: Project                 = p in file(".") also common
-    def sub: Project                  = p also common
-    def support: Project              = p also common
-    def sxr: Project                  = p configs SxrConfig settings (fullSxrSettings: _*)
+    def buildWith(vs: String*)        = p settings (crossScalaVersions := vs.toSeq)
+    def sub(text: String): Project    = p also (bintraySettings ++ mimaDefaultSettings ++ common) settings (name := "psp-" + p.id, description := text)
+    def support: Project              = p also common settings (publishArtifact := false)
     def also(ss: SettingSeq): Project = p settings (ss: _*)
+    def common = Seq[Setting[_]](
+                                  resolvers +=  "bintray/paulp" at "https://dl.bintray.com/paulp/maven",
+                               scalaVersion :=  "2.11.2",
+                         crossScalaVersions :=  Seq("2.10.4", "2.11.2"),
+                                    version :=  ( if (hasReleaseProp) baseVersion else localVersion ),
+                               organization :=  pspOrg,
+                                logBuffered :=  false,
+                                shellPrompt :=  (s => "%s#%s> ".format(name.value, s.currentProject)),
+                              scalacOptions ++= versionScalacOptions(scalaBinaryVersion.value),
+                               javacOptions ++= Seq("-nowarn", "-XDignore.symbol.file"),
+                                   licenses :=  Seq("Apache-2.0" -> url("http://www.apache.org/licenses/LICENSE-2.0")),
+                         logLevel in update :=  Level.Warn,
+                    publishArtifact in Test :=  false,
+                  parallelExecution in Test :=  false,
+                               fork in Test :=  true,
+                                 crossPaths :=  true,
+      unmanagedSourceDirectories in Compile +=  (sourceDirectory in Compile).value / s"scala${scalaBinaryVersion.value}",
+         unmanagedSourceDirectories in Test +=  (sourceDirectory in Test).value / s"scala${scalaBinaryVersion.value}"
+    )
+    def root: Project = p in file(".") also common settings (
+                 name :=  "psp-std-root",
+          description :=  "psp's project which exists to please sbt",
+      publishArtifact :=  false,
+             commands +=  Command.args("mima", "<version>")(mimaCommand),
+              publish <<= publish dependsOn prePublish,
+              console <<= console in Compile in consoleOnly,
+                 test <<= test in testOnly
+    )
   }
 
-  lazy val std = project.sub.sxr dependsOn api settings (
-                    name := "psp-std",
-             description := "psp's non-standard standard library",
-      crossScalaVersions := List("2.11.2")
-  )
-
-  lazy val api = project.sub.sxr settings (
-                   name := "psp-api",
-            description := "api for psp's non-standard standard library",
-     crossScalaVersions := List("2.10.4", "2.11.2")
-  )
+  lazy val root = project.root aggregate (api, std)
+  lazy val api  = project.sub("api for psp's non-standard standard library")
+  lazy val std  = project.sub("psp's non-standard standard library") dependsOn api settings (publishArtifact := scalaBinaryVersion.value == "2.11")
 }
 
-trait Versioning {
+trait PublishOnly {
+  self: Build.type =>
+
   import scala.sys.process._
 
-  def subprojects: List[Project]
+  lazy val baseVersion  = "0.3.0-M9"
+  lazy val localVersion = baseVersion + localSuffix
 
   // It's a pretty hideous version string, but it should guarantee we never overwrite
   // artifacts and that dependency managers will see the local version numbers as increasing.
@@ -117,39 +78,39 @@ trait Versioning {
   def repoDiff: String = ("git diff" #| "md5").!! take 10 // not using at present - hopefully "dirty" is enough
   def localSuffix      = "-%s-%s%s".format(dateTime, repoSha, if (isRepoClean) "" else "-dirty")
 
-  // TODO - Distinguish between milestones and releases.
-  def runPublish[A](key: TaskKey[A]) = Def.task[Unit] {
-    val state        = Keys.state.value
-    val isPublishKey = key eq publish
-    val suffix       = if (isPublishKey) "" else localSuffix
-
-    def finish()                                  = subprojects foreach (p => state(crossScalaVersions in p) foreach (cross => publishProject(p, cross)))
-    def publishProject(p: Project, cross: String) = state.set(version in p := state(version) + suffix, scalaVersion in p := cross) runTask (key in Compile in p)
-
-    // TODO - Require the git tag to exist (not for milestones) or create it.
-    if (!isPublishKey)
-      finish()
-    else if (!isRepoClean)
-      state err "Can't publish with a dirty repository."
+  def prePublish = Def.task[Boolean] {
+    if (!isRepoClean)
+      andFalse(state.value err "Can't publish with a dirty repository.")
     else if (!hasReleaseProp)
-      state err s"As a safeguard, publishing release artifacts requires -D$releaseProp."
+      andFalse(state.value err s"As a safeguard, publishing release artifacts requires -D$releaseProp.")
     else
-      finish()
+      true
   }
-
-  // Mima won't resolve the %% cross version.
-  def stdArtifact(version: String): ModuleID = pspOrg % "psp-std_2.11" % version
 }
 
 trait TestOnly {
   self: Build.type =>
+
+  /** mima command optionally takes a baseline version, e.g. sbt 'mima 0.1.0-M1'
+   */
+  def mimaCommand = stateCommand {
+    case (s, Nil)            => mimaRun(s, stdArtifact("0.3.0-M8"))
+    case (s, version :: Nil) => mimaRun(s, stdArtifact(version))
+    case (s, _)              => s.fail
+  }
+
+  private def mimaRun(state: State, module: ModuleID): Unit =
+    state.put(previousArtifact in std, Some(module)) runTask (reportBinaryIssues in std)
+
+  // Mima won't resolve the %% cross version.
+  def stdArtifact(version: String): ModuleID = pspOrg % "psp-std_2.11" % version
 
   def testDependencies = Def setting Seq(
     "org.scala-lang" % "scala-reflect" % scalaVersion.value,
     "org.scalacheck" %% "scalacheck" % "1.11.5" % "test"
   )
 
-  lazy val testOnly = project.support dependsOn std settings (
+  lazy val testOnly = project.support dependsOn (api, std) settings (
                    name  :=  "psp-test",
             description  :=  "test encapsulation",
     testOptions in Test  +=  Tests.Argument(TestFrameworks.ScalaCheck, "-verbosity", "1"),
@@ -173,7 +134,7 @@ trait ConsoleOnly {
       def !> : Unit = println(target)
       def >(implicit shows: Show[A]): Unit = println(target.to_s)
     }
-    import spire.algebra._, spire.math._, spire.implicits._, com.google.common.collect._
+    import spire.algebra._, spire.math._, com.google.common.collect._
   """
 
   def consoleDependencies = Def setting Seq(
@@ -186,9 +147,10 @@ trait ConsoleOnly {
 
   // A console project which pulls in misc additional dependencies currently being explored.
   lazy val consoleOnly = project.support dependsOn (std, api) settings (
-                          name  :=  "psp-console",
-                   description  :=  "console encapsulation",
-           libraryDependencies <++= consoleDependencies,
-    initialCommands in console  :=  consoleCode
+                                   name  :=  "psp-console",
+                            description  :=  "console encapsulation",
+                    libraryDependencies <++= consoleDependencies,
+    scalacOptions in console in Compile  :=  Seq("-language:_"),  // turning off all the noisy warnings
+             initialCommands in console  :=  consoleCode
   )
 }
--- project/Stated.scala
+++ project/Stated.scala
@@ -4,19 +4,23 @@ package build
 import sbt._, Keys._
 import scala.sys.process.Process
 
+final case class ProjectAndVersions(p: ProjectReference, versions: Seq[String])
+
 /** Encapsulation of the sbt craziness.
  */
 final class Stated(val state: State) {
   val extracted = Project extract state
   import extracted._
 
-  def currentRef   = extracted.currentRef
-  def settings     = structure.settings
-  def relation     = Project.relation(structure, true)
-  def scopedKeys   = relation._1s.toSeq
-  def attrKeys     = scopedKeys map (_.key)
-  def projectScope = Load projectScope currentRef
-  def remaining    = state.remainingCommands
+  def currentRef     = extracted.currentRef
+  def thisRef        = extracted get thisProjectRef
+  def currentProject = currentRef.project
+  def settings       = structure.settings
+  def relation       = Project.relation(structure, true)
+  def scopedKeys     = relation._1s.toSeq
+  def attrKeys       = scopedKeys map (_.key)
+  def projectScope   = Load projectScope currentRef
+  def remaining      = state.remainingCommands
 
   // def taskConfig = EvaluateTask.extractedTaskConfig(extracted, structure, state)
   // def evalTask[A](key: ScopedKey[Task[A]]) = EvaluateTask(structure, key, state, currentRef)
@@ -25,8 +29,11 @@ final class Stated(val state: State) {
   def getOpt[A](key: SettingKey[A]): Option[A] = extracted getOpt key
   def runTask[A](key: TaskKey[A])              = extracted.runTask(key, state)
 
+  def justRun[A](key: TaskKey[A])  = runTask(key)._1
+  def justEval[A](key: TaskKey[A]) = runTask(key)._2
+
   def put[A](key: SettingKey[A], value: A): Stated = set(key := value)
-  def runAll[A](key: TaskKey[A]): Stated           = new Stated(extracted.runAggregated(key, state))
+  def runAll[A](task: TaskKey[A]): Stated          = new Stated(extracted.runAggregated(task in thisRef, state))
   def set(settings: Setting[_]*): Stated           = new Stated(extracted.append(settings, state))
   def run(commands: String*): Stated               = new Stated(state.copy(remainingCommands = remaining ++ commands))
 
